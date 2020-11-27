288e16ad68605a2c0c92d599474068e1ac23fcf1
Kiet T. Tran, Ph.D <kiet123@gmail.com>
2020-11-27 12:46:23 -0500

    Removed sco dir

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package kata
 
+import (
+	"fmt"
+	"os"
+)
+
 // Main expects file name as the first argument (location 1).
 // By default Args[0] contains the name of the executable.
-//func main() {
-//	fmt.Println("bundle: main{}")
-//	if len(os.Args) < 2 {
-//		fmt.Println("*** error: no input file ***")
-//		os.Exit(-1)
-//	}
-//	data := readAll(os.Args[1])
-//	bundle := unmarshal(data)
-//	jsonData := marshalBundle(bundle)
-//	fmt.Printf("Bundle:%v\n", jsonData)
-//}
+func main() {
+	fmt.Println("bundle: main{}")
+	if len(os.Args) < 2 {
+		fmt.Println("*** error: no input file ***")
+		os.Exit(-1)
+	}
+	data := readAll(os.Args[1])
+	bundle := unmarshal(data)
+	jsonData := marshalBundle(bundle)
+	fmt.Printf("Bundle:%v\n", jsonData)
+}
 
 /**
  * Need:
